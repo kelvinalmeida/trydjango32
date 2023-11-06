25e7f03ee468d14f0ac18b5403d8585d3f32635b
Kelvin <kelvinsantosdealmeida@gmail.com>
2023-11-06 20:07:22 -0300

    form de criação de artigos

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import home, create_article_view
 
 urlpatterns = [
     path('', home, name='home'),
+    path('create/', create_article_view, name='create'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from .models import ArticleModel
 
+
 # Create your views here.
 def home(request, *args, **kwargs):
 
@@ -12,3 +13,21 @@ def home(request, *args, **kwargs):
     }
 
     return render(request, 'articles/home.html', context)
+
+
+def create_article_view(request, *args, **kwargs):
+    print(request.POST)
+    context = {
+        "object": None
+    }
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        object_article = ArticleModel.objects.create(title=title , content=content)
+        print(object_article)
+        context = {
+            "object": object_article
+        }
+
+    return render(request, 'articles/create.html', context)
